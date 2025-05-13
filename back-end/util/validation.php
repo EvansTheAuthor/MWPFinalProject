f55727406953cdcb03a76df73ab22938d1f43ff3
EvansTheAuthor <ibnumuhammadab19@gmail.com>
2025-05-14 06:31:46 +0700

    try to build a laravel backend

--- back-end/util/validation.php
+++
@@ -1,9 +0,0 @@
-<?php
-function validateEmail($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-function sanitize($string){
-    return htmlspecialchars(strip_tags($string));
-}
-?>
